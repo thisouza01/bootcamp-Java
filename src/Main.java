ea587eceed6c216e35a9649c5c0897bb011daae4
thisouza01 <tihsouza01@gmail.com>
2025-06-24 16:39:35 -0300

    alterando uma linha

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // importando
+        // importando o metodo Scanner para teste
         Scanner scanner;
     }
 }
