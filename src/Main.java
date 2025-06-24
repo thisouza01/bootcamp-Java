eefcca9ccb80f668892b582e089d7a93de7b2fda
thisouza01 <tihsouza01@gmail.com>
2025-06-24 16:53:09 -0300

    iniciando em JAVA

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 
+    private final static String WELCOME_MESSAGE = "Olá, informe o seu nome";
+
     public static void main(String[] args) {
         // importando o metodo Scanner para teste
-        Scanner scanner;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(WELCOME_MESSAGE);
+        String nome = scanner.next();
+
+        System.out.println("Olá, informe sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Olá " + nome + " sua idade é " + idade);
     }
 }
