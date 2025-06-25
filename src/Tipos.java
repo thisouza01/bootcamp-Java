93c3b72a498b0f0602edb7f6ecd29134a18a4b3a
thisouza01 <tihsouza01@gmail.com>
2025-06-24 22:42:09 -0300

    Tipos primitivos.

---
+++ src/Tipos.java
@@ -0,0 +1,11 @@
+public class Tipos {
+
+    public static void main(String[] args) {
+        var number = 1.34;
+        char caracter = 'a';
+        int numero = 12;
+        String palavra = "aqua";
+        var qual = true;
+        float flutuante = 1.42f;
+    }
+}
