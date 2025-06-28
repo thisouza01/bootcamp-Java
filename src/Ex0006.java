2212298c5889a40952b141025bb0397444b58a50
thisouza01 <tihsouza01@gmail.com>
2025-06-28 19:55:03 -0300

    Programa que recebe um numero ate que o resto da divisao dele pelo primeiro seja igual a zero.

---
+++ src/Ex0006.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ex0006 {
+    //informa um numero inicial
+    //entra em um loop para receber outros numeros, so para quando um numero digitado, dividido pelo primeiro informado
+    // tiver resto diferente de 0.
+    // numeros menores que o primeiro numero digitado devem ser ignorados
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero");
+        int number1 = scanner.nextInt();
+        int number2 = 0;
+        int resultDiv= 0;
+        do {
+            System.out.println("Digite outro numero");
+            resultDiv = 0;
+
+            if (number2 < number1) {
+                number2 = scanner.nextInt();
+            } else {
+                resultDiv = number2 % number1;
+            }
+        } while (resultDiv == 0);
+    }
+}
