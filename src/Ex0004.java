0e22cf0f834986cc914c537bbc13629c76f7a0a8
thisouza01 <tihsouza01@gmail.com>
2025-06-28 11:09:41 -0300

    lista de exercicios

---
+++ src/Ex0004.java
@@ -0,0 +1,19 @@
+public class Ex0004 {
+
+    public static void main(String[] args){
+        // calcula IMC - Peso / (altura * altura)
+        /* <= 18.5 -> abaixo do peso
+           > 18.5 && <= 24.9 -> peso ideal
+           > 25.9 && <= 29.9 -> Levemente obeso
+           > 29.9 && <= 34.4 -> Obeso
+           > 34.4            -> Obesidade morbida
+        */
+
+        // entre com um numero e um segundo numero maior - programa informa quais sao os impares e pares entre os dois numeros
+        // mostra numeros infomados e em ordem decrescente os numeros que foram gerados
+
+        //informa um numero inicial
+        //entra em um loop para receber outros numeros, so para quando um numero digitado, dividido pelo primeiro informado tiver resto diferente de 0.
+        // numeros menores que o primeiro numero digitado devem ser ignorados
+    }
+}
