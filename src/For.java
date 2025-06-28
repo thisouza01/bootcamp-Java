874a35d446d6babb3f699bc09b3eaa88a6b38f90
thisouza01 <tihsouza01@gmail.com>
2025-06-28 09:03:12 -0300

    Uso da estrutura 'for'.

---
+++ src/For.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class For {
+
+    public static void main(String[] args){
+        var scanner = new Scanner(System.in);
+        for (var i = 1; i < 10; i++){
+            System.out.println(i);
+        }
+    }
+}
